c178eb4283c2b71f13a7e74595b8f8b24f9f4e6e
Siddward <33522644+TripleS2307@users.noreply.github.com>
2021-01-07 12:56:21 -0600

    modified server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if(process.env.NODE_ENV === "production")
     app.use(express.static(__dirname + "/../public"));
 }
 app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname,'public', 'index.html'));
+	response.sendFile(path.join(__dirname,'/../public', 'index.html'));
 });
 // app.use(express.static(__dirname + '/../public'));
 app.use(express.static(distDir));
