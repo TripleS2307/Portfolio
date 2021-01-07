7ee1d50faa5dbce8e3f7fa39aeab740335165881
Siddward <33522644+TripleS2307@users.noreply.github.com>
2021-01-07 12:40:52 -0600

    modified server.js

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const port = process.env.PORT || 3000
 // Load Node modules
 var express = require('express');
 // Initialise Express
+var distDir = __dirname + "/dist/"
 var app = express();
 // Render static files
 app.use(express.static(__dirname + '/../public'));
+app.use(express.static(distDir));
 // Port website will run on
-app.listen(port);   
+app.listen(port);
